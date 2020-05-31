97f9697af9161a2636cf59d6042efb5f2b45f190
Aimanbibi786 <66163117+Aimanbibi786@users.noreply.github.com>
2020-05-31 13:26:24 +0500

    Update nozbar.py

--- nozbar.py
+++ nozbar.py
@@ -79,30 +79,30 @@ vuln = "\033[32mVuln"
 
 os.system("clear")
 print  """
-  \033[1;96m-┈┈SAQIB┈UR┈REHMAN┈┈┈┈┈┈         
+  \033[1;96m-┈┈AIMAN KHAN┈┈┈┈┈┈         
   \033[1;96m┈┈┈┈*┈┈┈┈┈┈┈┈┈┈┈┈┈┈*┈┈┈        
   \033[1;96m┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈*
  \033[1;96m ┈┈ اسلام علیکم ورحمتہ اللہ وبرکاتہ*┈┈
  \033[1;96m ┈┈┈┈*┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈*
  \033[1;96m ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
   \033[1;96m ─────────────•◈•──────────  
-   \033[1;92m███████▒▒WELCOME TO SAQIB CREATIONS▒▒████████
+   \033[1;92m███████▒▒WELCOME TO AIMAN CREATIONS▒▒████████
 \033[1;95m♡╭──────────•◈•──────────╮♡\033[1;96msaibkhan\033[1;95m♡╭──────────•◈•──────────╮♡
-\033[1;94mAuthor\033[1;91m: \033[1;91mSaqib Ur Rehman
+\033[1;94mAuthor\033[1;91m: \033[1;91mAiman khan
 \033[1;94mSaqibkhan\033[1;91m: \033[1;91▒▓██████████████]99.9
-\033[1;94mFacebook\033[1;91m: \033[1;91mSaqib ur Rehman
+\033[1;94mFacebook\033[1;91m: \033[1;91mAiman khan
 \033[1;94mWhatsapp\033[1;91m: \033[1;91m03484160325
-\033[1;95m♡╰──────────•◈•──────────╯♡\033[1;96mSaqibkhan\033[1;95m♡╰──────────•◈•──────────╯♡"""
-jalan('              \033[1;96m......SAQIB UR REHMAN.................:')
+\033[1;95m♡╰──────────•◈•──────────╯♡\033[1;96mAimankhan\033[1;95m♡╰──────────•◈•──────────╯♡"""
+jalan('              \033[1;96m......AIMAN KHAN.................:')
 jalan("\033[1;93m   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈   ")
 jalan('\033[1;93m   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈   ')
 jalan('\033[1;93m   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈   ')
 jalan("\033[1;93m   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ")
 jalan("\033[1;93m   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈")
-print "\033[1;93m♡─────saqib ur rehman•───────♡\033[1;96mLogin saqib\033[1;95m♡╰──────────•◈•──────────╯♡"
+print "\033[1;93m♡─────Aiman khan•───────♡\033[1;96mLogin saqib\033[1;95m♡╰──────────•◈•──────────╯♡"
 
-CorrectUsername = "saqib"
-CorrectPassword = "saqib"
+CorrectUsername = "Aiman"
+CorrectPassword = "Khan"
 
 loop = 'true'
 while (loop == 'true'):
@@ -209,7 +209,7 @@ def menu():
 	print "  \033[1;95m«-----♡----\033[1;93mLogged in User Info\033[1;95m----♡-----»"
 	print "	   \033[1;94m Name\033[1;93m:\033[1;92m"+nama+"\033[1;97m               "
 	print "	   \033[1;97m ID\033[1;93m:\033[1;92m"+id+"\x1b[1;97m              "
-	print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mSaqibKhanZ\033[1;95m♡─────────•◈•─────────♡"
+	print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mAimanKhan\033[1;95m♡─────────•◈•─────────♡"
 	print "\033[1;97m--\033[1;92m> \033[1;92m1.\x1b[1;92mStart Cloning..."
 	print "\033[1;97m--\033[1;91m> \033[1;91m0.\033[1;91mExit            "
 	pilih()
@@ -256,7 +256,7 @@ def pilih_super():
 	elif peak =="1":
 		os.system('clear')
 		print logo
-		print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mSaqib ur rehman\033[1;95m♡─────────•◈•─────────♡"
+		print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mAiman khan\033[1;95m♡─────────•◈•─────────♡"
 		jalan('\033[1;93mGetting IDs \033[1;97m...')
 		r = requests.get("https://graph.facebook.com/me/friends?access_token="+toket)
 		z = json.loads(r.text)
@@ -266,7 +266,7 @@ def pilih_super():
 		os.system('clear')
 		print logo
 		idt = raw_input("\033[1;96m[♡] \033[1;92mEnter ID\033[1;93m: \033[1;97m")
-		print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mSaqib\033[1;95m♡─────────•◈•─────────╯♡"
+		print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mAiman\033[1;95m♡─────────•◈•─────────╯♡"
 		try:
 			jok = requests.get("https://graph.facebook.com/"+idt+"?access_token="+toket)
 			op = json.loads(jok.text)
@@ -292,9 +292,9 @@ def pilih_super():
 	for o in titik:
 		print("\r\033[1;91mCloning\033[1;93m"+o),;sys.stdout.flush();time.sleep(1)
 	print "\n\033[1;94m«-----\x1b[1;93m♡To Stop Process Press CTRL+Z♡\033[1;94m----»"
-	print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mSaqibkhanz\033[1;95m♡─────────•◈•─────────♡"
+	print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mAimankhan\033[1;95m♡─────────•◈•─────────♡"
 	jalan(' \033[1;93m ........Cloning Start please Wait.......... ')
-	print "\033[1;95m♡─────────•◈•─────────♡\033[1;96msaqib\033[1;95m♡─────────•◈•─────────♡"
+	print "\033[1;95m♡─────────•◈•─────────♡\033[1;96mAiman\033[1;95m♡─────────•◈•─────────♡"
 	
 			
 	def main(arg):
